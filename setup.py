729339161eecacbe3f063e76d0e20b8f16bf851d
rbi_international <rohit.bharti8211@gmail.com>
2024-07-11 10:46:53 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rohit Bharti',
+    author_email= 'rohit.bharti8211@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
